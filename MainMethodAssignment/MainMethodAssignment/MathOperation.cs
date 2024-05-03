1bf624413ff010011d78abbdf44fe7af5e67bd52
BreGurney <150399227+BreGurney@users.noreply.github.com>
2024-05-03 15:16:13 -0700

    Add files via upload

---
+++ MainMethodAssignment/MainMethodAssignment/MathOperation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class MathOperation
+    {
+        public int MathOperation(int i, int j + 1)
+        {
+            int result = i * j;
+            return result;
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperation mathOp = new MathOperation();
+            Console.WriteLine("Enter an integer: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a second integer, or just press enter: ");
+            try
+            {
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                int resilts = mathOp.MathOperation(num1, num2);
+                Console.WriteLine(num1 + " x " + num2 + " = " + result);
+            }
+            catch
+            {
+                int results = mathOp.MathOperation(num1);
+                Console.WriteLine(num1 + " x Default 1 = " + results);
+            }
+            Console.ReadLine();
+        }
+    }
+}
