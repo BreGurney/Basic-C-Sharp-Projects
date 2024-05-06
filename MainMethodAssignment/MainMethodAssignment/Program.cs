0c4a8df2f57164a3fbb3d97e39faf65b03533f57
BreGurney <150399227+BreGurney@users.noreply.github.com>
2024-05-06 05:29:36 -1200

    Update Program.cs

--- MainMethodAssignment/MainMethodAssignment/Program.cs
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -17,12 +17,12 @@ namespace MainMethodAssignment
             try
             {
                 int num2 = Convert.ToInt32(Console.ReadLine());
-                int results = mathOp.MathOperation(num1, num2);
-                Console.WriteLine(num1 + " x " + num2 + " = " + result);
+                int results = mathOp.MathOperation1(num1, num2);
+                Console.WriteLine(num1 + " x " + num2 + " = " + results);
             }
             catch
             {
-                int results = mathOp.MathOperation(num1);
+                int results = mathOp.MathOperation1(num1);
                 Console.WriteLine(num1 + " x Default 1 = " + results);
             }
             Console.ReadLine();
