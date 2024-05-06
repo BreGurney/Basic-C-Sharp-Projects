4e5a321ca95275940be0dd4f789616f74e6b04e0
BreGurney <150399227+BreGurney@users.noreply.github.com>
2024-05-06 05:30:00 -1200

    Update MathOperation.cs

--- MainMethodAssignment/MainMethodAssignment/MathOperation.cs
+++ MainMethodAssignment/MainMethodAssignment/MathOperation.cs
@@ -8,7 +8,7 @@ namespace MainMethodAssignment
 {
     class MathOperation
     {
-        public int MathOperation(int i, int j = 1)
+        public int MathOperation1(int i, int j = 1)
         {
             int result = i * j;
             return result;
