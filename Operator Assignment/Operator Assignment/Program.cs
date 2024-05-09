8feb387ce6db892ef11781c0d1e7f5f43a12eb8d
BreGurney <150399227+BreGurney@users.noreply.github.com>
2024-05-09 13:52:03 -0700

    Operator Assignment
    The beginning of this assignment

---
+++ Operator Assignment/Operator Assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator_Assignment
+{
+    class Program : Employee
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.FirstName = "Bob";
+            employee.LastName = "Peters";
+            Employee employee1 = new Employee();
+            employee1.FirstName = "Breanne";
+            employee1.LastName = "Gurney";
+
+            Console.WriteLine(employee.FirstName + employee.ID);
+            Console.ReadLine();
+
+        }
+    }
+}
